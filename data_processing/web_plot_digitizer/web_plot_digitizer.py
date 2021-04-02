9aaaf92fd894318fb53528f743b8054b1c6efb66
Rory Lambert <rory.lambert@hotmail.co.uk>
2021-04-02 15:02:05 +0100

    Added ipynb tutorial for webplot_digitizer

--- data_processing/web_plot_digitizer/web_plot_digitizer.py
+++ data_processing/web_plot_digitizer/web_plot_digitizer.py
@@ -1,5 +1,4 @@
 
-#%%
 import pandas as pd
 import numpy as np
 
@@ -19,20 +18,3 @@ def read_wpd_outputs(ppath, dataname, xheading, yheading):
     #cast to numeric, ignore errors
     df = df.apply(pd.to_numeric, errors='ignore')    
     return df
-
-if __name__ == "__main__":
-    from pathlib import Path
-    import plotly.express as px
-
-    examplefiles = Path('./', 'data_processing/web_plot_digitizer/').rglob('*csv')
-
-    #I know these example files are for 495 and 950K
-    #read both into dfs, then concatenate together
-    output=[]
-    for f, mwt in zip(examplefiles, ['495K','950K']):
-        output.append(read_wpd_outputs(f, mwt, 'Spin speed (rpm)','Film Thickness (nm)'))
-    
-    df = pd.concat(output,axis=0)
-
-    #the output is in a good format for interfacing with plotly express
-    f = px.scatter(df, x='Spin speed (rpm)', y='Film Thickness (nm)', color='trace', facet_col='dataset')
